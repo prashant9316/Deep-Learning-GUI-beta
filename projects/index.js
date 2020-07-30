81e96e0e46d4d5e39d0628cf8a2c238f5eb26c65
Prashant Mishra <pgmd.9873@gmail.com>
2020-07-30 21:51:37 +0530

    Fixed a few typos in the project demo page

--- projects/index.js
+++ projects/index.js
@@ -1,15 +1,15 @@
 let container = document.getElementById('add');
-result = [{"Title":"Palm Angle Model", "Author":"Prashant Mishra","link":"https://prashant9316.github.io/"},
-			{"Title":"Project 2", "Author":"Sourav Mishra","link":"https://www.google.com"}]
+result = [{"Title":"Palm Angle", "Author":"Prashant","link":"https://prashant9316.github.io/"},
+			{"Title":"Project 2", "Author":"Sourav","link":"https://www.google.com"}]
 var len = 1;
 while(result.Title != null){
 	len += 1;
 }
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 12; i++) {
     let myDiv = document.createElement("div");
     myDiv.className = 'column';
     console.log("Adding Project Card " + String(i));
-    if (result.title!=null){
+    if (i<2){
     	myDiv.innerHTML = "<div class=\"card\"><h3>"+ result[i].Title +"</h3><p><a href="+result[i].link+">Author: "+result[i].Author+"</a></p><p>some text</p></div>" 
  	}
  	else {
