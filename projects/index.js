873f93d75d4302e3f28a86375c4b4abd6f2ef839
Prashant Mishra <pgmd.9873@gmail.com>
2020-07-30 23:23:01 +0530

    Added more features to make the project page responsive.

--- projects/index.js
+++ projects/index.js
@@ -17,3 +17,12 @@ for (let i = 0; i < 12; i++) {
  	}
     container.appendChild(myDiv);
 }
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
