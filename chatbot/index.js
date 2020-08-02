028288ddd369eb06db5f9a2cea26646d00283029
Prashant Mishra <pgmd.9873@gmail.com>
2020-08-02 14:29:56 +0530

    Chatbot Initial basic UI completed

--- chatbot/index.js
+++ chatbot/index.js
@@ -6,19 +6,19 @@ $("#user-input-form").on("submit", function(e) {
   
   var message = $("#user-input").val();
   
-  outputArea.append(`
-    <div class='bot-message'>
+  outputArea.append(`<div class="bot-message"><div class="us-message">${message}</div></div>`);
+    /*<div class='bot-message'>
       <div class='message'>
         ${message}
       </div>
     </div>
-  `);
+  `);*/
   
   setTimeout(function() {
     outputArea.append(`
       <div class='user-message'>
-        <div class='message'>
-          I'm like 20 lines of JavaScript I can't actually talk to you.
+        <div class='reply-message'>
+          yo man, what's up?
         </div>
       </div>
     `);
